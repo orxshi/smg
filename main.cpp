21edaf1919f355f6ba2c536279c87e5febd6361b
orxshi <orxan.shibli@gmail.com>
2023-02-22 14:21:36 +0200

    >

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include<vector>
+#include<array>
 #include<cmath>
 #include<iostream>
 #include<fstream>
 #include<matrix.h>
 
-const int nx = 2; // number of cells in x-direction
-const int ny = 2;
-const int nz = 1;
+// mesh resolution
+const int ni = 2; // number of cells in x-direction
+const int nj = 2;
+const int nk = 1;
 
-Vector3 p[nx+1][ny+1][nz+1];
+Vector3 p[ni+1][nj+1][nk+1];
 
 struct Line
 {
     Vector3 p0;
     Vector3 p1;
+
+    Line() = default;
 
     Line(Vector3& p0, Vector3& p1)
     {
@@ -70,6 +74,8 @@ struct IJK
     int j;
     int k;
 
+    IJK() = default;
+
     IJK(int i, int j, int k)
     {
         this->i = i;
@@ -84,42 +90,74 @@ int pos(IJK ijk)
     int j = ijk.j;
     int k = ijk.k;
 
-    return i + j * (nx+1) + k * (nx+1) * (ny+1) + 0;
+    return i + j * (ni+1) + k * (ni+1) * (nj+1) + 0;
 }
 
 struct Surface
 {
-    std::vector<IJK> ijk;
+    std::array<IJK, 4> ijk;
+    int bc;
 
-    Surface(std::vector<IJK> ijk)
+    Surface(IJK ijk0, IJK ijk1, IJK ijk2, IJK ijk3)
     {
-        this->ijk = ijk;
+        ijk[0] = ijk0;
+        ijk[1] = ijk1;
+        ijk[2] = ijk2;
+        ijk[3] = ijk3;
+    }
+};
+
+struct Face
+{
+    std::array<IJK, 4> ijk;
+
+    Face(IJK ijk0, IJK ijk1, IJK ijk2, IJK ijk3)
+    {
+        ijk[0] = ijk0;
+        ijk[1] = ijk1;
+        ijk[2] = ijk2;
+        ijk[3] = ijk3;
     }
 };
 
 struct Cell
 {
-    std::vector<IJK> ijk;
+    std::array<IJK, 8> ijk;
 
-    Cell(std::vector<IJK> ijk)
+    Cell() = default;
+
+    Cell(IJK ijk0, IJK ijk1, IJK ijk2, IJK ijk3, IJK ijk4, IJK ijk5, IJK ijk6, IJK ijk7)
     {
-        this->ijk = ijk;
+        ijk[0] = ijk0;
+        ijk[1] = ijk1;
+        ijk[2] = ijk2;
+        ijk[3] = ijk3;
+        ijk[4] = ijk4;
+        ijk[5] = ijk5;
+        ijk[6] = ijk6;
+        ijk[7] = ijk7;
     }
 };
 
-std::vector<Cell> cells;
-std::vector<Surface> surfaces;
-std::vector<Surface> faces;
+Cell cells[ni][nj][nk];
+
+Surface surfaces[ni][nj][2] IJ_surfaces;
+Surface surfaces[2][nj][nk] JK_surfaces;
+Surface surfaces[ni][2][nk] IK_surfaces;
+
+Face faces[ni][nj][nk-1] IJ_faces;
+Face faces[ni-1][nj][nk] JK_faces;
+Face faces[ni][nj-1][nk] IK_faces;
 
 void make_cells()
 {
-    for (int k=0; k<nz; ++k)
+    for (int k=0; k<nk; ++k)
     {
-        for (int j=0; j<ny; ++j)
+        for (int j=0; j<nj; ++j)
         {
-            for (int i=0; i<nx; ++i)
+            for (int i=0; i<ni; ++i)
             {
-                cells.push_back(std::vector<IJK>{IJK(i,j,k), IJK(i+1, j, k), IJK(i+1, j+1, k), IJK(i, j+1, k), IJK(i,j,k+1), IJK(i+1, j, k+1), IJK(i+1, j+1, k+1), IJK(i, j+1, k+1)});
+                cells[i][j][k] = Cell(IJK(i,j,k), IJK(i+1, j, k), IJK(i+1, j+1, k), IJK(i, j+1, k), IJK(i,j,k+1), IJK(i+1, j, k+1), IJK(i+1, j+1, k+1), IJK(i, j+1, k+1));
             }
         }
     }
@@ -127,211 +165,213 @@ void make_cells()
 
 void make_surfaces()
 {
-    for (int j=0; j<ny; ++j)
+    // IJ surfaces
+    for (int j=0; j<nj; ++j)
     {
-        for (int i=0; i<nx; ++i)
+        for (int i=0; i<ni; ++i)
         {
-            surfaces.push_back(std::vector<IJK>{IJK(i,j,0),  IJK(i+1, j, 0),  IJK(i+1, j+1, 0),  IJK(i, j+1, 0)});
-            surfaces.push_back(std::vector<IJK>{IJK(i,j,nz), IJK(i+1, j, nz), IJK(i+1, j+1, nz), IJK(i, j+1, nz)});
+            IJ_surfaces[i][j][0] = Surface(IJK(i,j,0), IJK(i+1, j, 0), IJK(i+1, j+1, 0), IJK(i, j+1, 0));
+            IJ_surfaces[i][j][1] = Surface(IJK(i,j,nk), IJK(i+1, j, nk), IJK(i+1, j+1, nk), IJK(i, j+1, nk));
         }
     }
 
-    for (int j=0; j<ny; ++j)
+    // JK surfaces
+    for (int j=0; j<nj; ++j)
     {
-        for (int k=0; k<nz; ++k)
+        for (int k=0; k<nk; ++k)
         {
-            surfaces.push_back(std::vector<IJK>{IJK(0,j,k),  IJK(0, j, k+1),  IJK(0, j+1, k+1),  IJK(0, j+1, k)});
-            surfaces.push_back(std::vector<IJK>{IJK(nx,j,k),  IJK(nx, j, k+1),  IJK(nx, j+1, k+1),  IJK(nx, j+1, k)});
+            JK_surfaces[0][j][k] = Surface(IJK(0,j,k), IJK(0, j, k+1), IJK(0, j+1, k+1), IJK(0, j+1, k));
+            JK_surfaces[1][j][k] = Surface(IJK(ni,j,k), IJK(ni, j, k+1), IJK(ni, j+1, k+1), IJK(ni, j+1, k));
         }
     }
 
-    for (int i=0; i<nx; ++i)
+    // IK surfaces
+    for (int i=0; i<ni; ++i)
     {
-        for (int k=0; k<nz; ++k)
+        for (int k=0; k<nk; ++k)
         {
-            surfaces.push_back(std::vector<IJK>{IJK(i,0,k),  IJK(i+1, 0, k),  IJK(i+1, 0, k+1),  IJK(i, 0, k+1)});
-            surfaces.push_back(std::vector<IJK>{IJK(i,ny,k),  IJK(i+1, ny, k),  IJK(i+1, ny, k+1),  IJK(i, ny, k+1)});
+            IK_surfaces[i][0][k] = Surface(IJK(i,0,k), IJK(i+1, 0, k), IJK(i+1, 0, k+1), IJK(i, 0, k+1));
+            IK_surfaces[i][1][k] = Surface(IJK(i,nj,k), IJK(i+1, nj, k), IJK(i+1, nj, k+1), IJK(i, nj, k+1));
         }
     }
 }
 
 void make_faces()
 {
-    for (int k=1; k<nz; ++k)
+    // IJ faces
+    for (int k=1; k<nk; ++k)
     {
-        for (int j=0; j<ny; ++j)
+        for (int j=0; j<nj; ++j)
         {
-            for (int i=0; i<nx; ++i)
+            for (int i=0; i<ni; ++i)
             {
-                faces.push_back(std::vector<IJK>{IJK(i,j,k), IJK(i+1, j, k), IJK(i+1, j+1, k), IJK(i, j+1, k)});
-		// parent: 
-		// if k-1 > 0
-		// 1: cell IJK(i,j,k-1)
-		// 2: cell IJK(i,j,k)
+                IJ_faces[i][j][k-1] = Face(IJK(i,j,k), IJK(i+1, j, k), IJK(i+1, j+1, k), IJK(i, j+1, k));
+                faces.push_back(Face(IJK(i,j,k), IJK(i+1, j, k), IJK(i+1, j+1, k), IJK(i, j+1, k)));
+                // parent: 
+                // if k-1 > 0
+                // 1: cell IJK(i,j,k-1)
+                // 2: cell IJK(i,j,k)
             }
         }
     }
 
-    for (int i=1; i<nx; ++i)
+    // JK faces
+    for (int i=1; i<ni; ++i)
     {
-	    for (int j=0; j<ny; ++j)
-	    {
-		    for (int k=0; k<nz; ++k)
-		    {
-			    faces.push_back(std::vector<IJK>{IJK(i,j,k),  IJK(i, j, k+1),  IJK(i, j+1, k+1),  IJK(i, j+1, k)});
+        for (int j=0; j<nj; ++j)
+        {
+            for (int k=0; k<nk; ++k)
+            {
+                faces.push_back(Face(IJK(i,j,k),  IJK(i, j, k+1),  IJK(i, j+1, k+1),  IJK(i, j+1, k)));
 
-		// parent: 
-		// if i-1 > 0
-		// 1: cell IJK(i-1,j,k)
-		// 2: cell IJK(i,j,k)
-		    }
-	    }
+                // parent: 
+                // if i-1 > 0
+                // 1: cell IJK(i-1,j,k)
+                // 2: cell IJK(i,j,k)
+            }
+        }
     }
 
-    for (int j=1; j<ny; ++j)
+    // IK faces
+    for (int j=1; j<nj; ++j)
     {
-	    for (int i=0; i<nx; ++i)
-	    {
-		    for (int k=0; k<nz; ++k)
-		    {
-			    faces.push_back(std::vector<IJK>{IJK(i,j,k),  IJK(i+1, j, k),  IJK(i+1, j, k+1),  IJK(i, j, k+1)});
+        for (int i=0; i<ni; ++i)
+        {
+            for (int k=0; k<nk; ++k)
+            {
+                faces.push_back(Face(IJK(i,j,k),  IJK(i+1, j, k),  IJK(i+1, j, k+1),  IJK(i, j, k+1)));
 
-		// parent: 
-		// if j-1 > 0
-		// 1: cell IJK(i,j-1,k)
-		// 2: cell IJK(i,j,k)
-		    }
-	    }
+                // parent: 
+                // if j-1 > 0
+                // 1: cell IJK(i,j-1,k)
+                // 2: cell IJK(i,j,k)
+            }
+        }
     }
-
 }
 
-int main()
+void generate_edge_points(Line* l)
 {
-    Vector3 p0(0.0, 0.0, 0.0);
-    Vector3 p1(1.0, 0.0, 0.0);
-    Vector3 p2(1.0, 1.0, 0.0);
-    Vector3 p3(0.0, 1.0, 0.0);
-
-    Vector3 p4(0.0, 0.0, 1.0);
-    Vector3 p5(1.0, 0.0, 1.0);
-    Vector3 p6(1.0, 1.0, 1.0);
-    Vector3 p7(0.0, 1.0, 1.0);
-
-    Line l0(p0, p1);
-    Line l1(p1, p2);
-    Line l2(p3, p2);
-    Line l3(p0, p3);
-
-    Line l4(p4, p5);
-    Line l5(p5, p6);
-    Line l6(p7, p6);
-    Line l7(p4, p7);
-
-    Line l8(p0, p4);
-    Line l9(p1, p5);
-    Line l10(p2, p6);
-    Line l11(p3, p7);
-
-
     // discretize l0 and l2 and l4 and l6
     {
-        auto pts0 = l0.generate(nx);
-        auto pts2 = l2.generate(nx);
-        auto pts4 = l4.generate(nx);
-        auto pts6 = l6.generate(nx);
-        for (int i=0; i<nx+1; ++i)
+        auto pts0 = l[0].generate(ni);
+        auto pts2 = l[2].generate(ni);
+        auto pts4 = l[4].generate(ni);
+        auto pts6 = l[6].generate(ni);
+        for (int i=0; i<ni+1; ++i)
         {
             p[i][0][0] = pts0[i];
-            p[i][ny][0] = pts2[i];
-            p[i][0][nz] = pts4[i];
-            p[i][ny][nz] = pts6[i];
+            p[i][nj][0] = pts2[i];
+            p[i][0][nk] = pts4[i];
+            p[i][nj][nk] = pts6[i];
         }
     }
 
     // discretize l1 and l3 and l5 and l7
     {
-        auto pts1 = l1.generate(ny);
-        auto pts3 = l3.generate(ny);
-        auto pts5 = l5.generate(ny);
-        auto pts7 = l7.generate(ny);
-        for (int j=0; j<ny+1; ++j)
+        auto pts1 = l[1].generate(nj);
+        auto pts3 = l[3].generate(nj);
+        auto pts5 = l[5].generate(nj);
+        auto pts7 = l[7].generate(nj);
+        for (int j=0; j<nj+1; ++j)
         {
             p[0][j][0] = pts3[j];
-            p[nx][j][0] = pts1[j];
-            p[0][j][nz] = pts5[j];
-            p[nx][j][nz] = pts7[j];
+            p[ni][j][0] = pts1[j];
+            p[0][j][nk] = pts5[j];
+            p[ni][j][nk] = pts7[j];
         }
     }
+}
 
-    // discretize l8 and l9 and l10 and l11
+void generate_surface_points()
+{
+    for (int i=1; i<ni; ++i)
     {
-        auto pts8 = l8.generate(nz);
-        auto pts9 = l9.generate(nz);
-        auto pts10 = l10.generate(nz);
-        auto pts11 = l11.generate(nz);
-        for (int k=0; k<nz+1; ++k)
+        for (int k=1; k<nk; ++k)
         {
-            p[0][0][k] = pts8[k];
-            p[nx][0][k] = pts9[k];
-            p[nx][ny][k] = pts10[k];
-            p[0][ny][k] = pts11[k];
+            Line l(p[i][0][0], p[i][0][nk]);
+            auto pts = l.generate(nk);
+            p[i][0][k] = pts[k];
         }
     }
+}
 
+void generate_inner_points()
+{
+    for (int i=0; i<ni+1; ++i)
     {
-        for (int i=1; i<nx; ++i)
+        for (int k=0; k<nk+1; ++k)
         {
-            for (int k=1; k<nz; ++k)
-            {
-                Line l(p[i][0][0], p[i][0][nz]);
-                auto pts = l.generate(nz);
-                p[i][0][k] = pts[k];
-            }
-        }
-    }
-
-    {
-        for (int i=1; i<nx; ++i)
-        {
-            for (int k=1; k<nz; ++k)
-            {
-                Line l(p[i][ny][0], p[i][ny][nz]);
-                auto pts = l.generate(nz);
-                p[i][ny][k] = pts[k];
-            }
-        }
-    }
-
-    for (int i=0; i<nx+1; ++i)
-    {
-        for (int k=0; k<nz+1; ++k)
-        {
-            Line l(p[i][0][k], p[i][ny][k]);
-            auto pts = l.generate(ny);
-            for (int j=1; j<ny; ++j)
+            Line l(p[i][0][k], p[i][nj][k]);
+            auto pts = l.generate(nj);
+            for (int j=1; j<nj; ++j)
             {
                 p[i][j][k] = pts[j];
             }
         }
     }
+}
 
-    make_surfaces();
-    make_cells();
-    make_faces();
-
+void print_custom()
+{
     int ncell = cells.size();
-    int ncell_xy = 2 * nx * ny;
-    int ncell_xz = 2 * nx * nz;
-    int ncell_yz = 2 * ny * nz;
-    int ncell_sur = ncell_xy + ncell_xz + ncell_yz;
-    int ncell_all = ncell + ncell_xy + ncell_xz + ncell_yz;
+    int ncell_ij = 2 * ni * nj;
+    int ncell_ik = 2 * ni * nk;
+    int ncell_jk = 2 * nj * nk;
+    int ncell_sur = ncell_ij + ncell_ik + ncell_jk;
+    int ncell_all = ncell + ncell_ij + ncell_ik + ncell_jk;
 
-    std::cout << "ncell: " << ncell << std::endl;
-    std::cout << "ncell_xy: " << ncell_xy << std::endl;
-    std::cout << "ncell_xz: " << ncell_xz << std::endl;
-    std::cout << "ncell_yz: " << ncell_yz << std::endl;
-    
+    std::ofstream out;
+    out.open("custom.mesh");
+
+    out << "POINTS ";
+    out << (ni+1) * (nj+1) * (nk+1) << std::endl;
+    for (int k=0; k<nk+1; ++k)
+    {
+        for (int j=0; j<nj+1; ++j)
+        {
+            for (int i=0; i<ni+1; ++i)
+            {
+                out << p[i][j][k](0) << " " << p[i][j][k](1) << " " << p[i][j][k](2) << std::endl;
+            }
+        }
+    }
+
+    out << "CELLS ";
+    out << cells.size() << std::endl;
+    for (int k=0; k<nk; ++k)
+    {
+        for (int j=0; j<nj; ++j)
+        {
+            for (int i=0; i<ni; ++i)
+            {
+                out << cell.ijk[0] << " ";
+                out << cell.ijk[1] << " ";
+                out << cell.ijk[2] << " ";
+                out << cell.ijk[3] << " ";
+                out << cell.ijk[4] << " ";
+                out << cell.ijk[5] << " ";
+                out << cell.ijk[6] << " ";
+                out << cell.ijk[7] << std::endl;
+            }
+        }
+    }
+
+    out << "IJ0 SURFACES ";
+    out << IJ0_surfaces.size() << std::endl;
+
+    out.close();
+}
+
+void print_vtk_str()
+{
+    int ncell = cells.size();
+    int ncell_ij = 2 * ni * nj;
+    int ncell_ik = 2 * ni * nk;
+    int ncell_jk = 2 * nj * nk;
+    int ncell_sur = ncell_ij + ncell_ik + ncell_jk;
+    int ncell_all = ncell + ncell_ij + ncell_ik + ncell_jk;
+
     std::ofstream out;
     out.open("str.vtk");
 
@@ -340,20 +380,20 @@ int main()
     out << "ASCII" << std::endl;
     out << "DATASET STRUCTURED_GRID" << std::endl;
     out << "DIMENSIONS " << std::endl;
-    out << nx+1;
+    out << ni+1;
     out << " ";
-    out << ny+1;
+    out << nj+1;
     out << " ";
-    out << nz+1 << std::endl;
+    out << nk+1 << std::endl;
     out << "POINTS ";
-    out << (nx+1) * (ny+1) * (nz+1);
+    out << (ni+1) * (nj+1) * (nk+1);
     out << " ";
     out << "float" << std::endl;
-    for (int k=0; k<nz+1; ++k)
+    for (int k=0; k<nk+1; ++k)
     {
-        for (int j=0; j<ny+1; ++j)
+        for (int j=0; j<nj+1; ++j)
         {
-            for (int i=0; i<nx+1; ++i)
+            for (int i=0; i<ni+1; ++i)
             {
                 out << p[i][j][k](0) << " " << p[i][j][k](1) << " " << p[i][j][k](2) << std::endl;
             }
@@ -361,8 +401,18 @@ int main()
     }
 
     out.close();
+}
 
+void print_vtk_ustr()
+{
+    int ncell = cells.size();
+    int ncell_ij = 2 * ni * nj;
+    int ncell_ik = 2 * ni * nk;
+    int ncell_jk = 2 * nj * nk;
+    int ncell_sur = ncell_ij + ncell_ik + ncell_jk;
+    int ncell_all = ncell + ncell_ij + ncell_ik + ncell_jk;
 
+    std::ofstream out;
     out.open("ustr.vtk");
 
     out << "# vtk DataFile Version 3.0" << std::endl;
@@ -370,14 +420,14 @@ int main()
     out << "ASCII" << std::endl;
     out << "DATASET UNSTRUCTURED_GRID" << std::endl;
     out << "POINTS ";
-    out << (nx+1) * (ny+1) * (nz+1);
+    out << (ni+1) * (nj+1) * (nk+1);
     out << " ";
     out << "float" << std::endl;
-    for (int k=0; k<nz+1; ++k)
+    for (int k=0; k<nk+1; ++k)
     {
-        for (int j=0; j<ny+1; ++j)
+        for (int j=0; j<nj+1; ++j)
         {
-            for (int i=0; i<nx+1; ++i)
+            for (int i=0; i<ni+1; ++i)
             {
                 out << p[i][j][k](0) << " " << p[i][j][k](1) << " " << p[i][j][k](2) << std::endl;
             }
@@ -423,6 +473,45 @@ int main()
     }
 
     out.close();
+}
+
+int main()
+{
+    // Corner points
+    Vector3 p0(0.0, 0.0, 0.0);
+    Vector3 p1(1.0, 0.0, 0.0);
+    Vector3 p2(1.0, 1.0, 0.0);
+    Vector3 p3(0.0, 1.0, 0.0);
+
+    Vector3 p4(0.0, 0.0, 1.0);
+    Vector3 p5(1.0, 0.0, 1.0);
+    Vector3 p6(1.0, 1.0, 1.0);
+    Vector3 p7(0.0, 1.0, 1.0);
+
+    // generate edges
+    Line* l = new Line[8];
+
+    l[0] = Line(p0, p1);
+    l[1] = Line(p1, p2);
+    l[2] = Line(p3, p2);
+    l[3] = Line(p0, p3);
+
+    l[4] = Line(p4, p5);
+    l[5] = Line(p5, p6);
+    l[6] = Line(p7, p6);
+    l[7] = Line(p4, p7);
+
+    generate_edge_points(l);
+    generate_surface_points();
+    generate_inner_points();
+    make_surfaces();
+    make_cells();
+    make_faces();
+    print_vtk_str();
+    //print_vtk_ustr();
+
+
+
 
     return 0;
 }
